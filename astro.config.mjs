185ea0f3aba97bb0b865566e470c2243c9c6b35d
sayo love <sayosarang@gmail.com>
2023-03-03 12:05:55 -0700

    github page workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
-		// Enable Preact to support Preact JSX components.
 		preact(),
-		// Enable React for the Algolia search component.
 		react(),
 	],
-	site: `https://astro.build`,
+	site: `https://sayolove.github.io`,
+	base: `/ngu-guide`,
 });
